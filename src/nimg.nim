789fa2cb84dda9ddc453429b29b17b46f4b1d5eb
Berkcan Uçan <hello@alca.dev>
2022-06-05 21:12:06 +0300

    added get image

--- src/nimg.nim
+++ src/nimg.nim
@@ -19,20 +19,35 @@ proc UploadImg*(ctx: Context) {.async.} =
     file = ctx.getUploadFile("image")
     var filename: string = file.filename
     file.save("/tmp", filename)
+    var tempPath = fmt("/tmp/{filename}")
     var ext = filename.split('.')
     var filterExt = filter(mimetypes, proc(x: string): bool = x.contains(ext[1]))
     if len(filterExt) <= 0:
       await ctx.respond(Http400, fmt("Mimetype must be one of following: {mimetypes}"))
     else:
       var destDir = os.getEnv("STORAGE_PATH")
-      copyFileToDir(fmt("/tmp/{filename}"), destDir)
-      removeFile(fmt("/tmp/{filename}"))
+      copyFileToDir(tempPath, destDir)
       await ctx.respond(Http200, "Success")
-  except:
-    await ctx.respond(Http400, "Key must be 'image'")
+
+    if fileExists(tempPath):
+      removeFile(tempPath)
+  except Exception:
+    var e = getCurrentExceptionMsg()
+    await ctx.respond(Http400, e)
+
+proc GetImg*(ctx: Context) {.async.} =
+  var storagePath = os.getEnv("STORAGE_PATH")
+  var fileParam = ctx.getPathParams("filename")
+  var filePath = fmt("{storagePath}/{fileParam}")
+
+  if fileExists(filePath):
+    await ctx.staticFileResponse(filePath, "")
+  else:
+    await ctx.respond(Http404, fmt("Image not found: {fileParam}"))
 
 when isMainModule:
   var app = newApp()
-  app.addRoute("/", Home)
-  app.addRoute("/upload", UploadImg, HttpPost, middlewares = @[auth()])
+  app.addRoute("/", Home, HttpGet)
+  app.addRoute("/upload", UploadImg, HttpPost)
+  app.addRoute("/{filename}", GetImg, HttpGet)
   app.run()
