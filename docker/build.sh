6671dfd1fd18de4eecdca2b6a886c10513c11d2f
Berkcan Uçan <hello@alca.dev>
2022-06-08 23:57:01 +0300

    updated build script

--- docker/build.sh
+++ docker/build.sh
@@ -6,15 +6,21 @@ else
   IMAGE_TAG="${NIMG_TAG}"
 fi
 
-echo "=> Build tag: ${NIMG_TAG}\n"
+printf "=> Build tag: %s\n" "${NIMG_TAG}"
+
+build() {
+  docker buildx build -t engineerhub/nimg:"$IMAGE_TAG" --platform linux/arm64,linux/amd64 -f docker/build.Dockerfile --push .
+  exit 1
+}
 
 if [[ $(docker buildx ls) =~ "multiarch" ]]; then
-  echo "=> Found build environment\n"
-  echo "=> Building image for linux/arm64,linux/amd64\n"
-  docker buildx build -t engineerhub/nimg:$IMAGE_TAG --platform linux/arm64,linux/amd64 -f docker/build.Dockerfile --push .
+  printf "=> Found build environment\n"
+  printf "=> Building image for linux/arm64,linux/amd64\n"
+  build
 else
-  echo "=> Creating build environment\n"
+  printf "=> Creating build environment\n"
   docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
   docker buildx create --name multiarch --driver docker-container --use
   docker buildx inspect --bootstrap
+  build
 fi
